341d93794bd47bbfd9085bb36f7077d86c52c2c1
rafaellevy <srlevy2@gmail.com>
2021-08-19 17:01:12 -0700

    Cache was created and tested

--- build/controllers/images.js
+++ build/controllers/images.js
@@ -5,18 +5,35 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var sharp_1 = __importDefault(require("sharp"));
 var path_1 = __importDefault(require("path"));
+// unit test for image processing will be code 201 when entry is created and 200 when entry already exist
+var cache = [];
 exports.default = {
     resize: function (req, res) {
         var _a = req.query, filename = _a.filename, height = _a.height, width = _a.width;
         //console.log("../assets/" + filename + ".jpg");
+        // find method takes a predicate ( it's a call back function)
+        var cacheEntry = cache.find(function (entry) { return entry.key === "" + filename + height + width; });
+        if (cacheEntry) {
+            res.send(cacheEntry.buffer);
+            console.log("Hit the cache");
+            return;
+        }
+        console.log("Did not hit the cache");
         try {
             // check in cache for filename with the given height and width
-            // ram or harddrive.
             sharp_1.default(path_1.default.join(__dirname, "../assets/" + filename + ".jpg"))
                 // sharp("../assets/" + filename + ".jpg")
                 .resize(Number(width), Number(height))
                 .toBuffer()
-                .then(function (img) { return res.send(img); });
+                .then(function (img) {
+                res.send(img);
+                // dynamic key
+                // cache[`${filename}${height}${width}`] = img;
+                cache.push({
+                    key: "" + filename + height + width,
+                    buffer: img,
+                });
+            });
         }
         catch (error) {
             console.error(error);
--- src/controllers/images.ts
+++ src/controllers/images.ts
@@ -1,18 +1,44 @@
 import sharp from "sharp";
 import path from "path";
 
+interface CacheEntry {
+  key: String;
+  buffer: Buffer;
+}
+
+// unit test for image processing will be code 201 when entry is created and 200 when entry already exist
+
+const cache: CacheEntry[] = [];
+
 export default {
   resize: (req: any, res: any) => {
     const { filename, height, width } = req.query;
     //console.log("../assets/" + filename + ".jpg");
+    // find method takes a predicate ( it's a call back function)
+    const cacheEntry: CacheEntry | undefined = cache.find(
+      (entry) => entry.key === `${filename}${height}${width}`
+    );
+    if (cacheEntry) {
+      res.send(cacheEntry.buffer);
+      console.log("Hit the cache");
+      return;
+    }
+    console.log("Did not hit the cache");
     try {
       // check in cache for filename with the given height and width
-      // ram or harddrive.
       sharp(path.join(__dirname, "../assets/" + filename + ".jpg"))
         // sharp("../assets/" + filename + ".jpg")
         .resize(Number(width), Number(height))
         .toBuffer()
-        .then((img: Buffer) => res.send(img));
+        .then((img: Buffer) => {
+          res.send(img);
+          // dynamic key
+          // cache[`${filename}${height}${width}`] = img;
+          cache.push({
+            key: `${filename}${height}${width}`,
+            buffer: img,
+          });
+        });
     } catch (error) {
       console.error(error);
       res.status(404).send("Image Not Found");
