4a821a05f2c8da5290bdccef1c35087353c3756b
rafaellevy <srlevy2@gmail.com>
2021-08-24 16:14:05 -0700

    commented jasmine code

--- build/tests/controllers/imagesSpec.js
+++ build/tests/controllers/imagesSpec.js
@@ -1,1 +1,15 @@
 "use strict";
+// import imageController from "../../controllers/images";
+// // it("expect myFunc(5) to equal 25", () => {
+// //   expect(myFunc(5)).toEqual(26);
+// // });
+// how to set this test with Jasmine
+// it("Response with cache on 2nd call", () => {
+// //   expect(imageController.resize({
+// //   query: {
+// //     filename: "",
+// //     height: "",
+// //     width: ""
+// //   }
+// // }))
+// });
--- src/tests/controllers/imagesSpec.ts
+++ src/tests/controllers/imagesSpec.ts
@@ -1,16 +1,17 @@
-import imageController from "../../controllers/images";
+// import imageController from "../../controllers/images";
 
-// it("expect myFunc(5) to equal 25", () => {
-//   expect(myFunc(5)).toEqual(26);
+// // it("expect myFunc(5) to equal 25", () => {
+// //   expect(myFunc(5)).toEqual(26);
+// // });
+
+// how to set this test with Jasmine
+// it("Response with cache on 2nd call", () => {
+// //   expect(imageController.resize({
+// //   query: {
+// //     filename: "",
+// //     height: "",
+// //     width: ""
+// //   }
+// // }))
+
 // });
-
-it("Response with cache on 2nd call", () => {
-  expect(imageController.resize({{
-  query: {
-    filename: "",
-    height: "",
-    width: ""
-  }
-}}))
-});
-
